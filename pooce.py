551e46c40ad5fe3237a04b8b16776e397c258e43
Peter Arato <it.arato@gmail.com>
2024-01-31 08:44:27 -0500

    add shell monitor

--- pooce.py
+++ pooce.py
@@ -4,6 +4,7 @@ import random
 import select
 import sys
 import numpy
+import subprocess
 
 OUT_WIDTH = 1280
 OUT_HEIGHT = 720
@@ -69,7 +70,6 @@ class StaticTextRenderPass(OutputRenderPass):
         self.text = text
 
     def render(self, img):
-        # TODO: Hflip.
         img = cv2.flip(img, 1)
 
         cv2.putText(
@@ -84,6 +84,46 @@ class StaticTextRenderPass(OutputRenderPass):
         )
 
         return cv2.flip(img, 1)
+
+
+class ShellWatcherRenderPass(OutputRenderPass):
+    def __init__(self, cmd_parts, frequency=10, x=OUT_WIDTH >> 1, y=OUT_HEIGHT >> 1):
+        self.cmd_parts = cmd_parts
+
+        self.frequency = frequency
+        self.counter = frequency
+        self.output = []
+
+        self.x = x
+        self.y = y
+
+    def render(self, img):
+        if self.counter >= self.frequency:
+            self.counter = 0
+
+            output_bytes = subprocess.check_output(self.cmd_parts)
+            output_utf8 = output_bytes.decode("utf-8")
+            self.output = output_utf8.split("\n")
+        else:
+            self.counter += 1
+
+        img = cv2.flip(img, 1)
+
+        for i, line in enumerate(self.output):
+            cv2.putText(
+                img,
+                line,
+                (self.x, self.y + (i * 30)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                COLOR_WHITE,
+                2,
+                cv2.LINE_AA,
+            )
+
+        img = cv2.flip(img, 1)
+
+        return img
 
 
 class TypingTextRenderPass(OutputRenderPass):
@@ -187,6 +227,7 @@ class VideoProxy(virtualvideo.VideoSource):
             StaticTextRenderPass("Pooce Demo v0"),
             TypingTextRenderPass(),
             PongRenderPass(),
+            ShellWatcherRenderPass(["vmstat"], 10, 8),
             # CarDrawRenderPass(),
         ]
 
