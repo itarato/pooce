82748923533b99ed1b85f57de8de8a7d9a1ea7cf
Peter Arato <it.arato@gmail.com>
2024-01-30 20:23:31 -0500

    add car recognition

--- pooce.py
+++ pooce.py
@@ -3,6 +3,7 @@ import cv2
 import random
 import select
 import sys
+import numpy
 
 OUT_WIDTH = 1280
 OUT_HEIGHT = 720
@@ -105,6 +106,52 @@ class TypingTextRenderPass(OutputRenderPass):
         return cv2.flip(img, 1)
 
 
+class CarDrawRenderPass(OutputRenderPass):
+    def __init__(self):
+        self.net = cv2.dnn.readNetFromCaffe(
+            "model/MobileNetSSD_deploy.prototxt",
+            "model/MobileNetSSD_deploy.caffemodel",
+        )
+        self.map = [0] * (OUT_HEIGHT * OUT_WIDTH)
+
+    def render(self, img):
+        blob = cv2.dnn.blobFromImage(img, 0.007843, (300, 300), 127.5)
+        h, w = img.shape[:2]
+        self.net.setInput(blob)
+        detections = self.net.forward()
+
+        for i in numpy.arange(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence <= 0.5:
+                continue
+
+            idx = int(detections[0, 0, i, 1])
+            if idx != 7:
+                continue
+
+            box = detections[0, 0, i, 3:7] * numpy.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # cv2.rectangle(
+            #     img, (startX, startY), (endX, endY), (0, 0, 255), -1, cv2.LINE_AA
+            # )
+            if (
+                startX >= 0
+                and startX < OUT_WIDTH
+                and startY >= 0
+                and startY < OUT_HEIGHT
+            ):
+                self.map[(startY * OUT_WIDTH) + startX] = 1
+
+        for y in range(OUT_HEIGHT):
+            for x in range(OUT_WIDTH):
+                if self.map[(y * OUT_WIDTH) + x] == 0:
+                    continue
+                cv2.circle(img, (x, y), 4, (0, 0, 255), -1, cv2.LINE_AA)
+
+        return img
+
+
 class VideoProxy(virtualvideo.VideoSource):
     def __init__(self):
         self.output_rect = (OUT_HEIGHT, OUT_WIDTH)
@@ -116,6 +163,7 @@ class VideoProxy(virtualvideo.VideoSource):
             StaticTextRenderPass("Pooce Demo v0"),
             TypingTextRenderPass(),
             PongRenderPass(),
+            CarDrawRenderPass(),
         ]
 
     def img_size(self):
