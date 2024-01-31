9ba24d2622f0133b899a70fd7a1e2cc35f48c626
Peter Arato <it.arato@gmail.com>
2024-01-31 09:22:46 -0500

    red dot drawing

--- pooce.py
+++ pooce.py
@@ -16,6 +16,7 @@ COLOR_BLACK = (0, 0, 0)
 COLOR_GREEN = (0, 255, 0)
 COLOR_BLUE = (255, 0, 0)
 COLOR_WHITE = (255, 255, 255)
+COLOR_PURPLE = (255, 0, 255)
 
 
 class OutputRenderPass:
@@ -216,6 +217,55 @@ class CarDrawRenderPass(OutputRenderPass):
         return img
 
 
+# https://github.com/ChristophRahn/red-circle-detection/blob/master/red-circle-detection.py
+class RedDotDrawRenderPass(OutputRenderPass):
+    def __init__(self):
+        self.map = [0] * (OUT_HEIGHT * OUT_WIDTH)
+
+    def render(self, img):
+        captured_frame = img
+
+        # Convert original image to BGR, since Lab is only available from BGR
+        captured_frame_bgr = cv2.cvtColor(captured_frame, cv2.COLOR_BGRA2BGR)
+        # First blur to reduce noise prior to color space conversion
+        captured_frame_bgr = cv2.medianBlur(captured_frame_bgr, 3)
+        # Convert to Lab color space, we only need to check one channel (a-channel) for red here
+        captured_frame_lab = cv2.cvtColor(captured_frame_bgr, cv2.COLOR_BGR2Lab)
+        # Threshold the Lab image, keep only the red pixels
+        # Possible yellow threshold: [20, 110, 170][255, 140, 215]
+        # Possible blue threshold: [20, 115, 70][255, 145, 120]
+        captured_frame_lab_red = cv2.inRange(
+            captured_frame_lab,
+            numpy.array([20, 150, 150]),
+            numpy.array([190, 255, 255]),
+        )
+        # Second blur to reduce more noise, easier circle detection
+        captured_frame_lab_red = cv2.GaussianBlur(captured_frame_lab_red, (5, 5), 2, 2)
+        # Use the Hough transform to detect circles in the image
+        circles = cv2.HoughCircles(
+            captured_frame_lab_red,
+            cv2.HOUGH_GRADIENT,
+            1,
+            captured_frame_lab_red.shape[0] / 8,
+            param1=100,
+            param2=18,
+            minRadius=5,
+            maxRadius=60,
+        )
+
+        if circles is not None:
+            circles = numpy.round(circles[0, :]).astype("int")
+            self.map[(OUT_WIDTH * circles[0, 1]) + circles[0, 0]] = 1
+
+        for y in range(OUT_HEIGHT):
+            for x in range(OUT_WIDTH):
+                if self.map[(y * OUT_WIDTH) + x] == 0:
+                    continue
+                cv2.circle(img, (x, y), 4, COLOR_PURPLE, -1, cv2.LINE_AA)
+
+        return img
+
+
 class VideoProxy(virtualvideo.VideoSource):
     def __init__(self):
         self.output_rect = (OUT_WIDTH, OUT_HEIGHT)
@@ -223,6 +273,7 @@ class VideoProxy(virtualvideo.VideoSource):
         self.videoInputOriginal = cv2.VideoCapture(IN_VIDEO_DEVICE_ID)
 
         self.output_render_passes = [
+            RedDotDrawRenderPass(),
             RandomFlashRenderPass(),
             StaticTextRenderPass("Pooce Demo v0"),
             TypingTextRenderPass(),
